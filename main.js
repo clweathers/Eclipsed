81a97ca2c5a4adc331c83c656519e47415ef35a7
Chris Weathers <clweathers1@gmail.com>
2024-07-09 23:46:07 -0400

    Probably pretty inefficient, but the effect looks nice!

--- main.js
+++ main.js
@@ -91,6 +91,22 @@ function draw() {
             particle.headingSpread = randomGaussian(0, 0.02) * PI / 20000;  // Subtle taper at the end
             //particle.headingSpread = randomGaussian(0, 0.02) * PI / 2000;   // Like confetti blowing around a fan
         });
+        particle_pool.activateNewParticle((particle) => {
+            let exit_ray = random(exit_rays);
+            particle.position = exit_ray.start_point.copy();
+            particle.velocity = p5.Vector.sub(exit_ray.end_point, exit_ray.start_point);
+            particle.velocity.setMag(random(2.9, 3.9));
+            particle.target_color = exit_ray.color;
+            particle.fadeout_duration = random(800, 1200);
+            particle.cooldown_duration = random(900, 1300);
+            particle.max_age = random(5000, 7500);
+            particle.birth_time = millis();
+
+            //particle.headingSpread = 0;                                     // Perfect lines
+            //particle.headingSpread = randomGaussian(0, 0.02) * PI / 30000;  // Subtle taper at the end
+            particle.headingSpread = randomGaussian(0, 0.02) * PI / 20000;  // Subtle taper at the end
+            //particle.headingSpread = randomGaussian(0, 0.02) * PI / 2000;   // Like confetti blowing around a fan
+        });
     }
 
     particle_pool.forEach((particle) => {
@@ -288,7 +304,7 @@ class Particle {
         
         fill(current_color);
 
-        circle(this.position.x, this.position.y, 5);
+        circle(this.position.x, this.position.y, 4);
     }
 
     get age() {
